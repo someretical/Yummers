c437c5259ba2ad4071b92893b4de0c7576b79345
someretical <29365738+someretical@users.noreply.github.com>
2023-05-18 23:04:15 +1000

    Add support for subcommand-only slash commands

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,14 +1,14 @@
-import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { CommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from 'discord.js';
 import BirthdayClient from './BirthdayClient';
 
 interface CommandOptions {
     client: BirthdayClient;
-    builder: SlashCommandBuilder;
+    builder: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
 }
 
 export default abstract class Command {
     public client: BirthdayClient;
-    public builder: SlashCommandBuilder;
+    public builder: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
 
     constructor(options: CommandOptions) {
         this.client = options.client;
