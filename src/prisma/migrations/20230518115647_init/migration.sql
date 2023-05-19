b2764e00cb23bb8fa4e6c9878c193d33e8b7c01d
someretical <29365738+someretical@users.noreply.github.com>
2023-05-19 23:10:53 +1000

    Updated User model

--- src/prisma/migrations/20230518115647_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Guild" ADD COLUMN     "birthdays_enabled" BOOLEAN NOT NULL DEFAULT false;
--- src/prisma/migrations/20230519092739_init/migration.sql
+++ src/prisma/migrations/20230519092739_init/migration.sql
@@ -1,8 +1,10 @@
 -- CreateTable
 CREATE TABLE "Guild" (
     "id" VARCHAR(32) NOT NULL,
+    "birthdays_enabled" BOOLEAN NOT NULL DEFAULT false,
     "birthday_role_id" VARCHAR(32),
     "birthday_channel_id" VARCHAR(32),
+    "birthday_message" VARCHAR(1024) NOT NULL DEFAULT '🎉🎉🎉 Happy Birthday {user}!',
 
     CONSTRAINT "Guild_pkey" PRIMARY KEY ("id")
 );
@@ -10,7 +12,12 @@ CREATE TABLE "Guild" (
 -- CreateTable
 CREATE TABLE "User" (
     "id" VARCHAR(32) NOT NULL,
-    "birthday" TIMESTAMP(3) NOT NULL,
+    "birthday_start_month" INTEGER NOT NULL,
+    "birthday_start_day" INTEGER NOT NULL,
+    "birthday_start_hour" INTEGER NOT NULL,
+    "birthday_start_minute" INTEGER NOT NULL,
+    "birthday_utc_offset" INTEGER NOT NULL,
+    "accept_birthday_messages" BOOLEAN NOT NULL DEFAULT true,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
