af4bd7631ec1293113df104f7f043d1d0f9240dd
someretical <29365738+someretical@users.noreply.github.com>
2023-05-19 23:13:39 +1000

    Added support with slash command with options

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,14 +1,25 @@
-import { CommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from 'discord.js';
+import {
+    CommandInteraction,
+    SlashCommandBuilder,
+    SlashCommandOptionsOnlyBuilder,
+    SlashCommandSubcommandsOnlyBuilder
+} from 'discord.js';
 import BirthdayClient from './BirthdayClient';
+
+type CommandBuilder =
+    | SlashCommandBuilder
+    | SlashCommandOptionsOnlyBuilder
+    | SlashCommandSubcommandsOnlyBuilder
+    | Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'>;
 
 interface CommandOptions {
     client: BirthdayClient;
-    builder: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+    builder: CommandBuilder;
 }
 
 export default abstract class Command {
     public client: BirthdayClient;
-    public builder: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+    public builder: CommandBuilder;
 
     constructor(options: CommandOptions) {
         this.client = options.client;
