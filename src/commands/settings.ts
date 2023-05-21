03f06d22dbbef3cf6a84daea9dac10a4be365ede
someretical <29365738+someretical@users.noreply.github.com>
2023-05-21 21:54:33 +1000

    Streamlined updating settings

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -42,247 +42,144 @@ export default class Settings extends Command {
                 .setDescription('View/set server specific settings')
                 .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageGuild)
                 .setDMPermission(false)
-                .addSubcommand((subcommand) => subcommand.setName('view').setDescription('View server settings'))
-                .addSubcommand((subcommand) =>
-                    subcommand
-                        .setName('birthdayfeatures')
-                        .setDescription('Enable birthday announcements')
-                        .addStringOption((option) =>
-                            option
-                                .setName('status')
-                                .setDescription('Enable/disable birthday announcements')
-                                .setRequired(true)
-                                .addChoices({ name: 'Enabled', value: '1' }, { name: 'Disabled', value: '0' })
-                        )
+                .addStringOption((option) =>
+                    option
+                        .setName('announcements')
+                        .setDescription('Enable/disable birthday announcements')
+                        .addChoices({ name: 'Enabled', value: '1' }, { name: 'Disabled', value: '0' })
                 )
-                .addSubcommand((subcommand) =>
-                    subcommand
-                        .setName('birthdaymessage')
-                        .setDescription('Set the birthday message.')
-                        .addStringOption((option) =>
-                            option
-                                .setName('message')
-                                .setDescription('The birthday message, use {user} to mention the user')
-                                .setRequired(true)
-                                .setMinLength(1)
-                                .setMaxLength(1024)
-                        )
+                .addStringOption((option) =>
+                    option
+                        .setName('message')
+                        .setDescription('The birthday message, use {user} to mention the user')
+                        .setMinLength(1)
+                        .setMaxLength(1024)
                 )
-                .addSubcommand((subcommand) =>
-                    subcommand
-                        .setName('birthdaychannel')
-                        .setDescription('Set the birthday announcement channel')
-                        .addChannelOption((option) =>
-                            option
-                                .setName('channel')
-                                .setDescription('The channel to send birthday announcements to')
-                                .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
-                                .setRequired(true)
-                        )
+                .addChannelOption((option) =>
+                    option
+                        .setName('channel')
+                        .setDescription('The channel to send birthday announcements to')
+                        .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
                 )
-                .addSubcommand((subcommand) =>
-                    subcommand
-                        .setName('birthdayrole')
-                        .setDescription('Set the birthday role')
-                        .addRoleOption((option) =>
-                            option
-                                .setName('role')
-                                .setDescription('The role to give to users on their birthday')
-                                .setRequired(true)
-                        )
+                .addRoleOption((option) =>
+                    option.setName('role').setDescription('The role to give to users on their birthday')
                 )
         });
     }
 
     async run(interaction: ChatInputCommandInteraction): Promise<void> {
         const guild = interaction.guild as Guild;
+        let pGuild: null | PrismaGuild = null;
 
-        switch (interaction.options.getSubcommand(true)) {
-            case 'view': {
-                let pGuild: null | PrismaGuild = null;
+        const enabled = interaction.options.getString('announcements')
+            ? interaction.options.getString('announcements') === '1'
+            : null;
+        const message = interaction.options.getString('message');
+        const channel: TextChannel | NewsChannel | null = interaction.options.getChannel('channel');
+        const role = interaction.options.getRole('role') as Role | null;
 
-                try {
-                    pGuild = await this.client.prisma.guild.upsert({
-                        where: {
-                            id: interaction.guildId as string
-                        },
-                        update: {},
-                        create: {
-                            id: interaction.guildId as string
-                        }
-                    });
-                } catch (err) {
-                    return databaseError(err, DatabaseErrorType.Write, interaction);
-                }
+        interface UpdateObject {
+            id?: string;
+            birthdays_enabled?: boolean;
+            birthday_message?: string;
+            birthday_channel_id?: string;
+            birthday_role_id?: string;
+        }
+        const upsertObject: UpdateObject = {};
+        if (enabled !== null) upsertObject['birthdays_enabled'] = enabled;
+        if (message) upsertObject['birthday_message'] = message;
 
-                const channel = guild.channels.cache.get(pGuild.birthday_channel_id ?? '0') as
-                    | BaseGuildTextChannel
-                    | undefined;
-                const channelPermissions = channel ? await validateChannel(channel) : false;
-
-                const role = guild.roles.cache.get(pGuild.birthday_role_id ?? '0') as Role | undefined;
-                const rolePermissions = role ? await validateRole(role) : false;
-
-                interaction.reply({
+        if (channel) {
+            if (!(await validateChannel(channel))) {
+                await interaction.reply({
                     embeds: [
-                        getEmbed()
-                            .setAuthor({
-                                name: guild.name,
-                                iconURL: guild.iconURL() as string
-                            })
-                            .addFields([
-                                {
-                                    name: 'Birthday announcements',
-                                    value: pGuild.birthdays_enabled ? 'Enabled' : 'Disabled'
-                                },
-                                {
-                                    name: 'Birthday channel',
-                                    value: `${channel?.toString() ?? 'Not set'} ${
-                                        channelPermissions || !channel ? '' : '(missing permissions)'
-                                    }`
-                                },
-                                {
-                                    name: 'Birthday role',
-                                    value: `${role?.toString() ?? 'Not set'} ${
-                                        rolePermissions || !role ? '' : ' (missing permissions)'
-                                    }`
-                                },
-                                {
-                                    name: 'Birthday message',
-                                    value: pGuild.birthday_message
-                                }
-                            ])
+                        getEmbed().setDescription(
+                            `The bot does not have the required permissions in ${channel.toString()}!`
+                        )
                     ]
                 });
-
-                break;
+                return;
             }
-            case 'birthdayfeatures': {
-                const enabled = interaction.options.getString('status', true) == '1';
 
-                try {
-                    await this.client.prisma.guild.upsert({
-                        where: {
-                            id: interaction.guildId as string
-                        },
-                        update: {
-                            birthdays_enabled: enabled
-                        },
-                        create: {
-                            id: interaction.guildId as string,
-                            birthdays_enabled: enabled
-                        }
-                    });
-                } catch (err) {
-                    return databaseError(err, DatabaseErrorType.Write, interaction);
-                }
-
-                interaction.reply({
-                    embeds: [getEmbed().setDescription(`${enabled ? 'Enabled' : 'Disabled'} birthday announcements.`)]
-                });
-
-                break;
-            }
-            case 'birthdaymessage': {
-                const message = interaction.options.getString('message', true);
-
-                try {
-                    await this.client.prisma.guild.upsert({
-                        where: {
-                            id: interaction.guildId as string
-                        },
-                        update: {
-                            birthday_message: message
-                        },
-                        create: {
-                            id: interaction.guildId as string,
-                            birthday_message: message
-                        }
-                    });
-                } catch (err) {
-                    return databaseError(err, DatabaseErrorType.Write, interaction);
-                }
-
-                interaction.reply({
-                    embeds: [getEmbed().setDescription('Updated birthday message.')]
-                });
-
-                break;
-            }
-            case 'birthdaychannel': {
-                const channel: TextChannel | NewsChannel = interaction.options.getChannel('channel', true);
-
-                if (!(await validateChannel(channel))) {
-                    await interaction.reply({
-                        embeds: [
-                            getEmbed().setDescription(
-                                `The bot does not have the required permissions in ${channel.toString()}!`
-                            )
-                        ]
-                    });
-                    return;
-                }
-
-                try {
-                    await this.client.prisma.guild.upsert({
-                        where: {
-                            id: interaction.guildId as string
-                        },
-                        update: {
-                            birthday_channel_id: channel.id
-                        },
-                        create: {
-                            id: interaction.guildId as string,
-                            birthday_channel_id: channel.id
-                        }
-                    });
-                } catch (err) {
-                    return databaseError(err, DatabaseErrorType.Write, interaction);
-                }
-
-                interaction.reply({
-                    embeds: [getEmbed().setDescription(`Set the birthday channel to ${channel.toString()}.`)]
-                });
-
-                break;
-            }
-            case 'birthdayrole': {
-                const role = interaction.options.getRole('role', true) as Role;
-
-                if (!(await validateRole(role))) {
-                    await interaction.reply({
-                        embeds: [
-                            getEmbed().setDescription(
-                                `The bot does not have the required permissions to manage ${role.toString()}!`
-                            )
-                        ]
-                    });
-                    return;
-                }
-
-                try {
-                    await this.client.prisma.guild.upsert({
-                        where: {
-                            id: interaction.guildId as string
-                        },
-                        update: {
-                            birthday_role_id: role.id
-                        },
-                        create: {
-                            id: interaction.guildId as string,
-                            birthday_role_id: role.id
-                        }
-                    });
-                } catch (err) {
-                    return databaseError(err, DatabaseErrorType.Write, interaction);
-                }
-
-                interaction.reply({
-                    embeds: [getEmbed().setDescription(`Set the birthday role to ${role.toString()}.`)]
-                });
-
-                break;
-            }
+            upsertObject['birthday_channel_id'] = channel.id;
         }
+
+        if (role) {
+            if (!(await validateRole(role))) {
+                await interaction.reply({
+                    embeds: [
+                        getEmbed().setDescription(
+                            `The bot does not have the required permissions to manage ${role.toString()}!`
+                        )
+                    ]
+                });
+                return;
+            }
+
+            upsertObject['birthday_role_id'] = role.id;
+        }
+
+        const copy = structuredClone(upsertObject);
+
+        interface CreateObject {
+            id: string;
+            birthdays_enabled?: boolean;
+            birthday_message?: string;
+            birthday_channel_id?: string;
+            birthday_role_id?: string;
+        }
+        copy['id'] = interaction.guildId as string;
+
+        try {
+            pGuild = await this.client.prisma.guild.upsert({
+                where: {
+                    id: interaction.guildId as string
+                },
+                update: upsertObject,
+                create: copy as CreateObject
+            });
+        } catch (err) {
+            return databaseError(err, DatabaseErrorType.Write, interaction);
+        }
+
+        const cacheChannel = guild.channels.cache.get(pGuild.birthday_channel_id ?? '0') as
+            | BaseGuildTextChannel
+            | undefined;
+        const channelPermissions = channel ? await validateChannel(channel) : false;
+
+        const cacheRole = guild.roles.cache.get(pGuild.birthday_role_id ?? '0') as Role | undefined;
+        const rolePermissions = role ? await validateRole(role) : false;
+
+        await interaction.reply({
+            embeds: [
+                getEmbed()
+                    .setAuthor({
+                        name: guild.name,
+                        iconURL: guild.iconURL() as string
+                    })
+                    .addFields([
+                        {
+                            name: 'Birthday announcements',
+                            value: pGuild.birthdays_enabled ? 'Enabled' : 'Disabled'
+                        },
+                        {
+                            name: 'Birthday channel',
+                            value: `${cacheChannel?.toString() ?? 'Not set'} ${
+                                channelPermissions || !channel ? '' : '(missing permissions)'
+                            }`
+                        },
+                        {
+                            name: 'Birthday role',
+                            value: `${cacheRole?.toString() ?? 'Not set'} ${
+                                rolePermissions || !role ? '' : ' (missing permissions)'
+                            }`
+                        },
+                        {
+                            name: 'Birthday message',
+                            value: pGuild.birthday_message
+                        }
+                    ])
+            ]
+        });
     }
 }
