5b8bda6b5b266bbdf45b650129c2df2fbce06716
someretical <29365738+someretical@users.noreply.github.com>
2023-05-26 16:27:14 +1000

    Merged all util functions into one file

---
+++ src/util.ts
@@ -0,0 +1,38 @@
+import { ChatInputCommandInteraction, ColorResolvable, EmbedBuilder } from 'discord.js';
+import { DateTime, FixedOffsetZone } from 'luxon';
+import Logger from './structures/Logger';
+
+export enum DatabaseErrorType {
+    Read = 'reading',
+    Write = 'writing'
+}
+
+export async function databaseError(err: Error, type: DatabaseErrorType, interaction: ChatInputCommandInteraction) {
+    Logger.err(`Database error`);
+    Logger.err(err);
+
+    await interaction.reply({
+        embeds: [getEmbed().setTitle('Database Error').setDescription(`There was an error while ${type} the database!`)]
+    });
+}
+
+export function getEmbed(): EmbedBuilder {
+    return new EmbedBuilder().setColor(process.env.EMBED_COLOUR as ColorResolvable);
+}
+
+export function stringToBirthday(dateString: string, offset: number, year = 2000): DateTime {
+    const zone = FixedOffsetZone.instance(offset);
+
+    const birthday = DateTime.fromObject(
+        {
+            year: year,
+            month: parseInt(dateString.substring(0, 2)),
+            day: parseInt(dateString.substring(2, 4)),
+            hour: parseInt(dateString.substring(4, 6)),
+            minute: parseInt(dateString.substring(6))
+        },
+        { zone: zone }
+    ).plus({ minutes: zone.offset(0) });
+
+    return birthday;
+}
--- src/util/EmbedHelper.ts
+++
@@ -1,5 +0,0 @@
-import { ColorResolvable, EmbedBuilder } from 'discord.js';
-
-export function getEmbed(): EmbedBuilder {
-    return new EmbedBuilder().setColor(process.env.EMBED_COLOUR as ColorResolvable);
-}
